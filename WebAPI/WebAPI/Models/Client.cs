764047da1868ada9e0a2d8b74785cb48f2ec44df
ThabangStix22 <mojaperets@gmail.com>
2022-11-25 21:35:41 +0200

    Added Models

---
+++ WebAPI/WebAPI/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAPI.Data
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public string CL_Email_Address { get; set; }
+
+        public int CL_Cell_Number { get; set; }
+
+        public bool CL_Gender { get; set; }
+
+        public string CL_Affidavit { get; set; }
+
+        public string CL_Password { get; set; }
+
+        public string CL_Role { get; set; }
+    }
+}
